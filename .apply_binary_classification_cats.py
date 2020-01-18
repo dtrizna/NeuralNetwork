a2c392b9f0b51e79c1839b6eded61f7e7bc986ec
Dmitrijs Trizna <dtrizna@gmail.com>
2020-01-18 21:53:57 +0200

    'README'

--- .apply_binary_classification_cats.py
+++ .apply_binary_classification_cats.py
@@ -11,16 +11,16 @@ def load_data():
     train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
     train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
 
-    test_dataset = h5py.File('datasets/binary_classification_cats/test_catvnoncat.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+    dev_dataset = h5py.File('datasets/binary_classification_cats/test_catvnoncat.h5', "r")
+    dev_set_x_orig = np.array(dev_dataset["test_set_x"][:]) # your dev set features
+    dev_set_y_orig = np.array(dev_dataset["test_set_y"][:]) # your dev set labels
 
-    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    classes = np.array(dev_dataset["list_classes"][:]) # the list of classes
     
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    dev_set_y_orig = dev_set_y_orig.reshape((1, dev_set_y_orig.shape[0]))
     
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+    return train_set_x_orig, train_set_y_orig, dev_set_x_orig, dev_set_y_orig, classes
 
 
 def gradient_check(train_x, train_y):
@@ -41,7 +41,7 @@ def see_cat(idx, train_x_orig, train_y, classes):
     
 
 def main(to_see_cat = False):
-    train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+    train_x_orig, train_y, dev_x_orig, dev_y, classes = load_data()
 
     if to_see_cat:
         while True:
@@ -56,14 +56,14 @@ to see it OR anything else to start training: ".format(len(train_x_orig)-1))
                 break
 
     # NORMALIZE FEATURES
-    # Reshape the training and test examples
+    # Reshape the training and dev examples
     train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T   
     # The "-1" makes reshape flatten the remaining dimensions
-    test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+    dev_x_flatten = dev_x_orig.reshape(dev_x_orig.shape[0], -1).T
 
     # Standardize data to have feature values between 0 and 1.
     train_x = train_x_flatten/255.
-    test_x = test_x_flatten/255.
+    dev_x = dev_x_flatten/255.
     # END NORMALIZE
 
     # CAN BE ENABLED FOR BACKPROP VERIFICATION
@@ -74,14 +74,15 @@ to see it OR anything else to start training: ".format(len(train_x_orig)-1))
     # WITHOUT REGULARIZATION
     #trained_weights = train(train_x, train_y, parameters, 1501, learning_rate=0.02)
 
-    # WITH L2 REGULARIZATION
-    trained_weights = train(train_x, train_y, parameters, 1501, learning_rate=0.02, lambd=0.7)
-
-    print("Prediction on Train set:")
-    predict(train_x, train_y, trained_weights)
-    print("Prediction on Test set:")
-    predict(test_x, test_y, trained_weights)
+    # WITH L2 REGULARIZATION - SEEKING BEST LAMBDA
+    for l in [0.01, 0.1, 0.7, 1, 1.5, 3, 10]:
+        trained_weights = train(train_x, train_y, parameters, 1501, learning_rate=0.02, lambd=l, print_cost=False)
+        print("Lambda value: {}".format(l))
+        #print("Prediction on Train set:")
+        #predict(train_x, train_y, trained_weights)
+        print("Prediction on Dev set:")
+        predict(dev_x, dev_y, trained_weights)
 
 
 if __name__ == "__main__":
-    main(True)
+    main(False)
--- README.md
+++ README.md
@@ -3,29 +3,79 @@
 
 Neural Network (NN) interface is localed at train.train()  
 To train network with your dataset:  
-- initialize NN (defines NN architecture):  
+1. initialize NN (defines NN architecture):  
   * using `initialization.initialize_parameters_random()`  
   * in case of Deep Networks (5+ hidden layers), you may use `initializaiton.initialize_parameters_he()` in order to overcome Vanishing/Exploding weight problem.  
-  Example:  
+  *Example*:  
     initialization of Binary Classification NN with 2 hidden layers 5 activation units each:  
         `parameters = initialization.initialize_parameters_random([X.shape[0], 5, 5, 1])`
+2. Normalize features (TODO)
+3. call `train.train()` by feeding into Train data set (X) and it's labeled data (Y)
+  *Usage Notes!*
+  * Find correct `learning_rate` by using `print_cost=True`. If:
+    - CF value is increasing:
+      + choose lower `learning_rate`
+    - CF value is decreasing, but slowly:
+      + choose higher `learning_rate`
+      + verify if features are correctly normalized
+  * If you supply `lambd` value to function, it will implement L2 Regularization.
+  * If you supple `keep_prob` value to function, it will implement DropOut Regularization (TODO).
+4. Tune Parameters/Hyperparameters on Dev set.
+5. Analyze algorithm performance on Test set.
+    - TODO: Use F1 Score 
 
-# Binary Classification Results
+# Binary Classification Example
 
 ## Dataset "binary_classification_cats"
 
+Code: `.apply_binary_classification_cats.py`  
+
 ### Training without Regularization
 
+```
 Cost after iteration 1500: 0.02472818901125718  
 Prediction on Train set:  
 Accuracy: 0.9952153110047844  
-Prediction on Test set:  
-Accuracy: 0.6600000000000001  
+Prediction on Dev set:  
+Accuracy: 0.6600000000000001
+```
+
+*Optimization Rationale:*
+Algorithm does well on Training set, thus there's no big Avoidable Bias (assuming that Human and Bayer errors are close to 0%). Therefore, Bias reduction tecnhiques as:  
+- training more Deep/Complex network
+- improving quality of pictures
+- longer training
+- Gradient Descent optimizations (e.g. Adam)
+... won't help, assuming that Train and Dev sets come from same distribution (which indeed are).  
+  
+There's huge gat between Dev and Test set results, which is due to Variance. So:  
+- regularization
+- more training data (which gives better generalization)
+.. should help.
 
 ### Training with L2 Regularization (lambda = 0.7)
 
-Cost after iteration 1500: 0.05416693578711332  
-Prediction on Train set:  
-Accuracy: 0.9999999999999998  
-Prediction on Test set:  
-Accuracy: 0.78  
+```
+Predictions on Dev set:
+
+Lambda value: 0.01
+Accuracy: 0.70
+
+Lambda value: 0.1
+Accuracy: 0.74
+
+Lambda value: 0.7
+Accuracy: 0.72
+
+Lambda value: 1
+Accuracy: 0.72
+
+Lambda value: 1.5
+Accuracy: 0.72
+
+Lambda value: 3
+Accuracy: 0.68
+
+Lambda value: 10
+Accuracy: 0.68
+```
